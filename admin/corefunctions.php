c977cf14813785f915c1297cc9d6ab2b544601b7
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2008-08-12 09:43:42 +0000

    Added clean() function to prevent SQL injection

--- admin/corefunctions.php
+++ admin/corefunctions.php
@@ -10,6 +10,18 @@ $stselect = sqlite_query($handle, "SELECT * FROM coreinfo") or die("SQLite query
 		$cmsinfo[$row['variable']] = stripslashes(stripslashes($row['value']));
 	}
 	
+function clean($var) {
+  $replace = array(
+    '&' => '&amp;',
+    '"' => '&quot;',
+    "'" => '&#39;',
+    '<' => '&lt;',
+    '>' => '&gt;'
+  );
+  $var = str_replace(array_keys($replace), array_values($replace), $var);
+  return $var;
+}
+	
 $grav_default=$site_url."admin/style/gravatar.gif";
 $gravatar = "http://www.gravatar.com/avatar.php?gravatar_id=".md5($_SESSION['email'])."&amp;default=".urlencode($grav_default)."&amp;size=60";
 ?>
