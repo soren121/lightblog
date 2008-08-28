984d2a2be4ee941612c3144848a3371d98f1f9cc
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2008-08-28 10:07:35 +0000

    Bug fixes

--- admin/corefunctions.php
+++ admin/corefunctions.php
@@ -16,7 +16,7 @@ $site_url = $cmsinfo['site_url'];
 
 // SQL queries
 $result01 = sqlite_query($handle, "SELECT * FROM posts ORDER BY id desc") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
-$result02 = sqlite_query($handle, "SELECT * FROM comments WHERE post_id=".$post->id."") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
+//$result02 = sqlite_query($handle, "SELECT * FROM comments WHERE post_id=".$post->id."") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
 $result03 = sqlite_query($handle, "SELECT * FROM posts WHERE id=".$_GET['id']." ORDER BY id desc") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
 $result04 = sqlite_query($handle, "SELECT * FROM comments WHERE post_id=".$_GET['id']." ORDER BY id desc") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
 $result05 = sqlite_query($handle, "SELECT * FROM pages WHERE id=".$_GET['id']." ORDER BY id desc") or die("SQLite query error: code 02<br>".sqlite_error_string(sqlite_last_error($handle)));
