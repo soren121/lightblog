9ab9f1a591ece4c9633e957f070ebded57e5ea5d
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2008-08-28 22:26:25 +0000

    Fixed installer bug

--- install.php
+++ install.php
@@ -19,8 +19,8 @@ if(isset($_POST['step1'])) {
 	sqlite_query($dbhle, "INSERT INTO coreinfo VALUES('site_url','".$site_url."')");
 	sqlite_query($dbhle, "CREATE TABLE comments(id integer primary key,post_id int(11) NOT NULL default '0',username text NOT NULL,email text NOT NULL,website text NOT NULL,text text NOT NULL)");
 	sqlite_query($dbhle, "CREATE TABLE pages(id INTEGER NOT NULL PRIMARY KEY DEFAULT '0',title TEXT NOT NULL,page TEXT NOT NULL)");
-	sqlite_query($dbhle, "CREATE TABLE posts(id INTEGER NOT NULL PRIMARY KEY DEFAULT '0',title TEXT NOT NULL,post TEXT NOT NULL,date TEXT NOT NULL,author TEXT NOT NULL,catid id INTEGER NOT NULL default '1')");
-	sqlite_query($dbhle, "INSERT INTO posts VALUES(1,'Welcome to LightBlog!','Welcome to LightBlog! We hope you enjoy it!<br /><br />-The LightBlog Team<br />http://lightblog.googlecode.com/',1218324266,'LightBlog Devs')");
+	sqlite_query($dbhle, "CREATE TABLE posts(id INTEGER NOT NULL PRIMARY KEY DEFAULT '0',title TEXT NOT NULL,post TEXT NOT NULL,date TEXT NOT NULL,author TEXT NOT NULL,catid INTEGER NOT NULL default '1')");
+	sqlite_query($dbhle, "INSERT INTO posts VALUES(1,'Welcome to LightBlog!','Welcome to LightBlog! We hope you enjoy it!<br /><br />-The LightBlog Team<br />http://lightblog.googlecode.com/',1218324266,'LightBlog Devs',1)");
 	sqlite_query($dbhle, "CREATE TABLE users(id INTEGER NOT NULL PRIMARY KEY DEFAULT '0',username TEXT NOT NULL,password TEXT NOT NULL,email TEXT NOT NULL,realname TEXT NOT NULL,vip INTEGER NOT NULL,ip TEXT NOT NULL,openid TEXT)");
 	sqlite_close($dbhle);
 	$ccp = fopen("config.php", 'w') or die("Unable to write to configuration file.");
