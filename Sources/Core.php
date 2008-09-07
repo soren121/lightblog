fae0392c7dffc01505449f916bd5fe2f23ff2d8a
Nicholas Narsing <soren121@sorenstudios.com>
2008-09-07 12:03:28 +0000

    [rewrite] Removed template compiler in favor of PHP, added jQuery libs

--- Sources/Core.php
+++ Sources/Core.php
@@ -25,19 +25,27 @@ if($lighty_installed == "false") {
 	header('Location: install.php');
 }
 
+// This function loads a language (obviously :P)
+function loadLanguage(strtolower(ucwords($input))) {
+	return include($language_dir.$input.'.language.php');
+}
+
 // This function compiles and loads themes
-function loadTemplate(strtolower(ucwords($pagename))) {
+function loadTemplate(strtolower(ucwords($input))) {
+	// Make the template path easier to read ;)
+	$current_theme_dir = $theme_dir.$current_theme.'/';
 	// First, we want to check if a template for this page exists
-	if(file_exists($theme_dir.$current_theme.'/'.$page.'.template.php')) {
-		// Open up the template compiler
-		require_once($sources_dir.'TemplateCompiler.php');
-		// Start up the template compiler class
-		$page = new Page($pagename);
-		// Replace and compile template tags
-		$page->replace_tags(array(
-		));
-		// Output page in pure (X)HTML
-		return $page->output();
+	if(file_exists($current_theme_dir.$input.'.template.php')) {
+		// Open up the template
+		require_once($current_theme_dir.$input.'.template.php');
+		// Tack on the header, sidebar, and footer and send it out!
+		loadLanguage($current_language);
+		$header = include($current_theme_dir.'Header.template.php');
+		$sidebar = include($current_theme_dir.'Sidebar.template.php');
+		$page = include($current_theme_dir.$input.'.template.php');
+		$footer = include($current_theme_dir.'Footer.template.php');
+		$output = echo $header.$sidebar.$page.$footer;
+		return $output;
 	}
 }
 
