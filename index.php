d255aed31f6ce463befc689b17703de72fb9c9fe
Nicholas Narsing <soren121@sorenstudios.com>
2008-10-18 22:06:42 +0000

    Installation checker added

--- index.php
+++ index.php
@@ -22,11 +22,15 @@ require_once('./config.php');
 require_once($sources_dir. '/Startup.php');
 
 // Are we doing an ?act, ?page, or what?
+// And is LightBlog even installed?
 if(isset($_GET['act']) && $core->isAction($_GET['act'])) {
   $core->loadAction($_GET['page']);
 }
 elseif(!empty($_GET['page'])) {
   $core->loadPage($_GET['page']);
+}
+elseif($lighty_installed == false) {
+  header('Location: Install.php');
 }
 else {
   $core->loadIndex();
