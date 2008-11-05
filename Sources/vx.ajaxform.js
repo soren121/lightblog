daf473aebe02cb95595f25e5a2246e9348eeddce
Nicholas Narsing <soren121@sorenstudios.com>
2008-11-05 22:10:30 +0000

    Updated AJAX form plugin, vX, included vX util plugin

--- Sources/vx.ajaxform.js
+++ Sources/vx.ajaxform.js
@@ -1,6 +1,3 @@
-/* vX AJAX Post Form JS plugin by soren121 */
-_.Y=function(c,r){var t=_.G(c),z={},y=t.getElementsByTagName("*");
-for(var i=y.length;i--;) z[y[i].name]=y[i].value
-_.X(t.action,r,'?'+_.Q(z),t.method=="post")
-}
-function ajaxifyForm(f){_.E(_.G(f),"submit",function(e){e.preventDefault();_.Y(f)})}
+/* vX AJAX Post Form JS plugin by soren121 & antimatter15 */
+_.ajaxsubmit=function(c,r){var t=_.G(c),z={},y=t.getElementsByTagName("*");for(var i=y.length;i--;) z[y[i].name]=y[i].value;_.X(t.action,r,'?'+_.Q(z),t.method=="post")}
+_.ajaxform=function(f,r){_.E(_.G(f),"submit",function(e){e.preventDefault();_.ajaxsubmit(f,r)})}
