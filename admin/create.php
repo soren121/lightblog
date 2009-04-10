ca32cc31f63984b97dd53c6b0cf37d64c29930c3
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2009-04-10 15:42:37 +0000

    Updated manage.php, as of now unstyled

--- admin/create.php
+++ admin/create.php
@@ -60,12 +60,12 @@ elseif((int)$_GET['type'] == 2) { $type = 'page'; }
 					url: this.getAttribute('action'),
 					timeout: 2000,
 					error: function() {
-						$('.notifybox').text('Failed to submit <?php echo ucwords($type) ?>.').css("background","#b20000").slideDown("normal");
+						$('#notifybox').text('Failed to submit <?php echo ucwords($type) ?>.').css("background","#b20000").slideDown("normal");
 						console.log("Failed to submit");
 						alert("Failed to submit.");
 					},
 					success: function(r) {
-						$('.notifybox').html('<?php echo ucwords($type) ?> created. | <' + 'a href="' + r + '">View post</' + 'a>').slideDown("normal");
+						$('#notifybox').html('<?php echo ucwords($type) ?> created. | <' + 'a href="' + r + '">View <?php echo $type ?></' + 'a>').slideDown("normal");
 					}
 				})
 				return false;
@@ -113,7 +113,7 @@ elseif((int)$_GET['type'] == 2) { $type = 'page'; }
 			use the navigation bar above to choose the correct type.</p>
 			<?php else: ?>
 			<h2 class="title"><img class="textmid" src="style/create.png" alt="" />Add New <?php echo ucwords($type) ?></h2>
-			<div class="notifybox"></div>
+			<div id="notifybox"></div>
 			<form action="<?php bloginfo('url') ?>Sources/ProcessAJAX.php" method="post" id="create">
 				<p><input class="hint textfield cf" name="title" type="text" title="Title" /></p>
 				<p><textarea rows="12" cols="36" name="text" id="wysiwyg"></textarea></p>
--- admin/manage.php
+++ admin/manage.php
@@ -20,69 +20,97 @@
 require('../config.php');
 require(ABSPATH .'/Sources/Core.php');
 
-$result01 = $dbh->query("SELECT * FROM ".$_GET['type']."s ORDER BY id desc") or die(sqlite_error_string($dbh->lastError));
-?>
-<!--	LightBlog v0.9.0
-		Copyright 2009 soren121. Some Rights Reserved.
-		Licensed under the General Public License v3.
-		For more info, see the LICENSE.txt file included.
--->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+if((int)$_GET['type'] == 1) { $type = 'post'; }
+elseif((int)$_GET['type'] == 2) { $type = 'page'; }
 
+$result = $dbh->query("SELECT * FROM ".$type."s ORDER BY id desc") or die(sqlite_error_string($dbh->lastError));
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title><?php echo bloginfo('title'); ?></title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Geany 0.13/soren121" />
-	<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
-	<!--[if IE]>
-	<link rel="stylesheet" href="style/iefix.css" type="text/css" media="screen" />
-	<![endif]-->
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Manage <?php echo ucwords($type) ?>s - <?php bloginfo('title') ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('url') ?>admin/style/style.css" />
+	<!--[if lte IE 7]><style type="text/css">html.jqueryslidemenu { height: 1%; }</style><![endif]-->
+	<script type="text/javascript" src="<?php bloginfo('url') ?>Sources/jQuery.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('url') ?>Sources/jQuery.SlideMenu.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('url') ?>Sources/jQuery.Corners.js"></script>
+	<script type="text/javascript">			
+		$(document).ready(function(){ 
+			$('.rounded').corner(); 
+			$('.roundedt').corner("round top 10px"); 
+			$('.roundedb').corner("round bottom 10px");
+		});
+	</script>
 </head>
 
 <body>
-<div id="container">
-	<div id="header">
-		<div id="headerimg">
-			<img class="headerimg" src="style/title.png" alt="LightBlog" />
+	<div id="wrapper">
+		<div id="header" class="roundedt">
+			<a href="<?php bloginfo('url') ?>"><?php bloginfo('title') ?></a>	 
 		</div>
+        <div id="navigation" class="jqueryslidemenu">
+			<ul>
+				<li><a href="dashboard.php">Dashboard</a></li>
+				<li><a href="javascript:void(0)">Create</a>
+					<ul>
+						<li><a href="create.php?type=1">Post</a></li>
+						<li><a href="create.php?type=2">Page</a></li>
+					</ul>
+				</li>
+				<li><a href="javascript:void(0)">Manage</a>
+					<ul>
+						<li><a href="manage.php?type=1">Post</a></li>
+						<li><a href="manage.php?type=2">Page</a></li>
+					</ul>
+				</li>
+				<li><a href="design.php">Design</a></li>
+				<li><a href="javascript:void(0)">Users</a>
+					<ul>
+						<li><a href="users.php">Manage Users</a></li>
+						<li><a href="profile.php">Your Profile</a></li>
+					</ul>
+				</li>
+				<?php if(userFetch('role', 'r') >= 1): ?>
+				<li><a href="settings.php">Settings</a></li>
+				<?php endif; ?>
+				<li><a href="login.php?logout=yes">Logout</a></li>
+			</ul>
+		</div>
+		<div id="content">
+			<!-- Check if parameters were set -->
+			<?php if(!isset($type)): ?>
+			<p>The type of content to manage was not specified. You must have taken a bad link. Please
+			use the navigation bar above to choose the correct type.</p>
+			<!-- They were, so continue -->
+			<?php else: ?>
+			<h2 class="title"><img class="textmid" src="style/edit.png" alt="" />Manage <?php echo ucwords($type) ?>s</h2>
+			<!-- Check if any posts/pages exist -->
+			<?php if($result->numRows() > 0): ?>
+			<table class="managelist">
+				<!-- Start row loop -->
+				<?php while($post = $result->fetchObject()): ?>
+				<tr>
+					<td><?php echo $post->id ?></td>
+					<td><?php echo $post->title ?></td>
+					<?php if($type == 'post'): ?>
+						<td><?php echo $post->author ?></td>
+						<td><?php echo date('n/j/Y', $post->date) ?></td>
+					<?php endif; ?>
+				</tr>
+				<!-- End row loop -->
+				<?php endwhile; ?>
+			</table>
+			<!-- None exist error message -->
+			<?php else: ?>
+			<p>Sorry, no <?php echo $type ?>s exist to manage.</p>
+			<!-- End both ifs -->
+			<?php endif; endif; ?>
+		</div>
+		<div id="footer" class="roundedb">		
+			Powered by LightBlog <?php LightyVersion() ?>    
+	    </div>
 	</div>
-	<?php include('admside.php'); ?>
-	<div id="content">
-	<h2>Manage <?php echo $_GET['type'] ?>s</h2><br />
-	<div id="postlist">
-	<?php if($_SESSION['role'] <= 0 or !(isset($_SESSION['role']))): ?>
-	Hey, you shouldn't even be in here! <a href="javascript:history.go(-2)">Go back to where you came from.</a>
-	<?php else:
-	if ($result01->numRows() > 0) {
-		echo'<table class="postlist">'; 
-		// start post loop
-		while($post = $result01->fetchObject()) {
-			// timestamp for date
-			$timestamp = $post->date;
-			// start row
-			echo '<tr>';
-			// output ID
-			echo '<td class="postlist">'.$post->id.'</td>';
-			// output title
-			echo '<td class="postlist">'.$post->title.'</td>';
-			// output author name & date
-			echo '<td class="postlist">'.$post->author.'</td>';
-			echo '<td class="postlist">'.date("n/j/Y", $timestamp).'</td>';
-			// output edit & delete links
-			echo '<td class="postlist"><a style="text-align: center;" href="edit.php?id='.$post->id.'&amp;type='.$_GET['type'].'">Edit</a></td>';
-			echo '<td class="postlist"><a style="text-align: center;" href="delete.php?id='.$post->id.'&amp;type='.$_GET['type'].'">Delete</a></td>';
-			// end row
-			echo '</tr>';
-			// this code is repeated for every post in your database
-		}
-		echo '</table>';
-	}
-	else { echo "No ".$_GET['type']."s, sorry."; } 
-	endif; ?>
-	</div>
-	</div>
-</div>
 </body>
 </html>
