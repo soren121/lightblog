34dcce8bbffeb09681676e0e518c9669b3b2549f
Nicholas Narsing <soren121@sorenstudios.com>
2009-04-10 23:53:13 +0000

    Fixed SQL injection issues, started styling manage.php

--- post.php
+++ post.php
@@ -21,8 +21,8 @@ require('config.php');
 require(ABSPATH .'/Sources/Core.php');
 
 // Request post and comments from database
-$result03 = $dbh->query("SELECT * FROM posts WHERE id=".$_GET['id']." ORDER BY id desc") or die(sqlite_error_string($dbh->lastError));
-$result04 = $dbh->query("SELECT * FROM comments WHERE post_id=".$_GET['id']." ORDER BY id asc") or die(sqlite_error_string($dbh->lastError));
+$result03 = $dbh->query("SELECT * FROM posts WHERE id=".(int)$_GET['id']." ORDER BY id desc") or die(sqlite_error_string($dbh->lastError));
+$result04 = $dbh->query("SELECT * FROM comments WHERE post_id=".(int)$_GET['id']." ORDER BY id asc") or die(sqlite_error_string($dbh->lastError));
 $result10 = $dbh->query("SELECT * FROM pages ORDER BY id desc") or die(sqlite_error_string($dbh->lastError));
 
 // Include theme files
