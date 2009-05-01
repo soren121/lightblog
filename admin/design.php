7e240fef791e3f172f101e8ed62d537edf32af13
Nicholas Narsing <soren121@sorenstudios.com>
2009-05-01 20:52:02 +0000

    Fixed theme selector, row hiding

--- admin/design.php
+++ admin/design.php
@@ -82,7 +82,7 @@ require(ABSPATH .'/Sources/Core.php');
 					<p class="label"><label for="changetheme">Available themes</label></p>
 					<p><select name="changetheme" id="changetheme">
 						<?php $dir = dirlist(ABSPATH .'/themes'); foreach($dir as $k => $v): ?>
-							<option value="<?php echo $v ?>"><?php echo $v ?></option>
+							<option value="<?php echo $k ?>"><?php echo $v ?></option>
 						<?php endforeach; ?>
 					</select></p>
 					<p><input type="submit" name="themesubmit" id="themesubmit" value="Save" /></p>
