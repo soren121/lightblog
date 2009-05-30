afcfcf6e8ae8dff04fdae7aefcf9dfb43a3a3c06
Nicholas Narsing <soren121@sorenstudios.com>
2009-05-30 23:42:13 +0000

    Fixed comment form styling bugs, page.php behavior

--- themes/default/page.php
+++ themes/default/page.php
@@ -6,16 +6,6 @@
 						<?php echo unescapeString($page->title); ?>
 					</h4>
 					<p class="post"><?php echo unescapeString($page->page); ?></p>
-					<div class="postdata">
-						<span class="postdata">
-							<img src="<?php bloginfo('url') ?>themes/<?php bloginfo('theme') ?>/style/date.png" alt="" />
-							<?php echo date('F j, Y', $page->date); ?>
-						</span>
-						<span class="postdata">
-							<img src="<?php bloginfo('url') ?>themes/<?php bloginfo('theme') ?>/style/user.png" alt="" />
-							<?php echo $page->author; ?>
-						</span>
-					</div>
 				</div>
 				<!-- End the loop -->			
 				<?php endwhile; endif; ?>
