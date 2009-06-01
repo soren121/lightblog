56e4a8d7d5eb8327a1651ec9cc78e615f70f026c
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2009-06-01 12:57:48 +0000

    Added Akismet settings page, improved form styling

--- admin/menu.php
+++ admin/menu.php
@@ -34,14 +34,19 @@
 					</ul>
 				</li>
 				<li><a href="design.php">Design</a></li>
-				<li><a href="javascript:void(0)">Users</a>
+				<li><a href="users.php">Users</a>
 					<ul>
 						<li><a href="users.php">Manage Users</a></li>
 						<li><a href="profile.php">Your Profile</a></li>
 					</ul>
 				</li>
 				<?php if(userFetch('role', 'r') >= 1): ?>
-				<li><a href="settings.php">Settings</a></li>
+				<li><a href="settings.php">Settings</a>
+					<ul>
+						<li><a href="settings.php">General Settings</a></li>
+						<li><a href="settings_akismet.php">Akismet Settings</a></li>
+					</ul>
+				</li>
 				<?php endif; ?>
 				<li><a href="login.php?logout=yes">Logout</a></li>
 			</ul>
