17046723bf079fa521066582c6745624c9b5f396
Nicholas Narsing <soren121@sorenstudios.com>
2009-07-08 19:09:46 +0000

    Various bugfixes; wrote excerpt function

--- themes/default/head.php
+++ themes/default/head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<title><?php bloginfo('title') ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('url') ?>themes/<?php bloginfo('theme') ?>/style/style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('themeurl') ?>/style/style.css" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('url') ?>feed.php"  title="<?php bloginfo('title') ?> RSS Feed" />
 	<link rel="alternate" type="application/atom+xml" href="<?php bloginfo('url') ?>feed.php?type=atom"  title="<?php bloginfo('title') ?> Atom Feed" />
 	<script type="text/javascript" src="<?php bloginfo('url') ?>Sources/jQuery.js"></script>
