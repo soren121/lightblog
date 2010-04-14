949d2a2ce0c3bd792f621bc7dd6a6ef8e200e36a
Nicholas Narsing <soren121@sorenstudios.com>
2010-04-14 22:33:11 +0000

    Removed InputHint, now using <label>s; updated Roadmap

--- Sources/jQuery.InputHint.js
+++
@@ -1,3 +0,0 @@
-jQuery.fn.hint=function(blurClass){if(!blurClass){blurClass='blur';}
-return this.each(function(){var $input=jQuery(this),title=$input.attr('title'),$form=jQuery(this.form),$win=jQuery(window);function remove(){if($input.val()===title&&$input.hasClass(blurClass)){$input.val('').removeClass(blurClass);}}
-if(title){$input.blur(function(){if(this.value===''){$input.val(title).addClass(blurClass);}}).focus(remove).blur();$form.submit(remove);$win.unload(remove);}});};
