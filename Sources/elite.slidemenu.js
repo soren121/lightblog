32db367850baa37cb9d55e61f422983fb6b587ba
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2010-04-15 22:42:09 +0000

    Worked on elite.slidemenu

--- Sources/elite.slidemenu.js
+++ Sources/elite.slidemenu.js
@@ -4,30 +4,21 @@
 	Ported to the elite.js library by The LightBlog Team
 ******************************************************************************************/
 
-var menu = function() {
-	var t = 15, z = 50, s = 6, a;
-	function dd(n) {
-		this.n = n;
-		this.h = [];
-		this.c = [];
+$.fn.menu = function(id) {
+	var a = c, s= $(id).tag('ul'), l = $(s).length, i = 0, this.n = n, this.h = [], this.c = [];
+	for(i; i < l; i++) {
+		var h = s[i].parentNode;
+		this.h[i] = h;
+		this.c[i] = s[i];
+		h.onmouseover = new Function(this.n+'.st('+i+',true)');
+		h.onmouseout = new Function(this.n+'.st('+i+')');
 	}
-	dd.prototype.init = function(p, c) {
-		a = c;
-		var w = document.getElementById(p), s = w.getElementsByTagName('ul'), l = s.length, i = 0;
-		for(i; i < l; i++){
-			var h = s[i].parentNode;
-			this.h[i] = h;
-			this.c[i] = s[i];
-			h.onmouseover = new Function(this.n+'.st('+i+',true)');
-			h.onmouseout = new Function(this.n+'.st('+i+')');
-		}
-	}
-	dd.prototype.st = function(x, f) {
-		var c = this.c[x], h = this.h[x], p = h.getElementsByTagName('a')[0];
+	function st(x, f) {
+		var c = this.c[x], h = this.h[x], p = $(h).tag('a')[0];
 		clearInterval(c.t);
 		c.style.overflow = 'hidden';
 		if(f) {
-			p.className += ' ' + a;
+			$(p).addClass(a);
 			if(!c.mh) {
 				c.style.display = 'block';
 				c.style.height = '';
@@ -44,7 +35,7 @@ var menu = function() {
 			}
 		}
 		else {
-			p.className = p.className.replace(a, '');
+			$(p).removeClass(a);
 			c.t = setInterval(function() { sl(c,-1); }, t);
 		}
 	}
@@ -52,17 +43,16 @@ var menu = function() {
 		var h = c.offsetHeight;
 		if((h <= 0 && f != 1) || (h >= c.mh && f==1)) {
 			if(f == 1) {
-				c.style.filter='';
-				c.style.opacity=1;
-				c.style.overflow='visible';
+				$(c).show();
 			}
 			clearInterval(c.t);
 			return;
 		}
-		var d = (f == 1) ? Math.ceil((c.mh - h)/s) : Math.ceil(h/s), o = h/c.mh;
-		c.style.opacity = o;
-		c.style.filter = 'alpha(opacity='+(o*100)+')';
-		c.style.height = h+(d*f)+'px';
+		if(f == 1) {
+			$(c).fade(1).slide(1);
+		}
+		else {
+			$(c).fade(0).slide(0);
+		}
 	}
-	return {dd:dd}
-}();
+};
