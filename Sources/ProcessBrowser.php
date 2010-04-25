3ce50300823ef3a3c83b564a1171680178cbd1ac
Nicholas Narsing <soren121@sorenstudios.com>
2010-04-25 03:58:58 +0000

    Finished PID enhancement started in r461

--- Sources/ProcessBrowser.php
+++ Sources/ProcessBrowser.php
@@ -22,24 +22,25 @@ require(ABSPATH .'/Sources/Core.php');
 
 // Process comment submission
 if(isset($_POST['comment_submit'])) {
-	if(bloginfo('comment_moderation', 1) === 'none') {
-		if(strlen($_POST['comment_name']) && strlen($_POST['comment_email']) > 0) {
-				// Escape values
-				$comment_pid = intval($_POST['comment_pid']);
-				$comment_name = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_name'])));
-				$comment_email = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_email'])));
-				$comment_website = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_website'])));
-				$comment_date = time();
-				$comment_text = sqlite_escape_string(cleanHTML($_POST['comment_text']));
+	if(strlen($_POST['comment_name']) && strlen($_POST['comment_email']) > 0) {
+			// Escape values
+			$comment_pid = (int)$GLOBALS['pid'];
+			$comment_name = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_name'])));
+			$comment_email = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_email'])));
+			$comment_website = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_website'])));
+			$comment_date = time();
+			$comment_text = sqlite_escape_string(cleanHTML($_POST['comment_text']));
+			if(bloginfo('comment_moderation','r') == 'approval') {
+				// Submit the comment
+				$dbh->query("INSERT INTO comments (published,pid,name,email,website,date,text) VALUES(0,$comment_pid,'$comment_name','$comment_email','$comment_website',$comment_date,'$comment_text')") or die(sqlite_error_string($dbh->lastError));
+			}
+			if(bloginfo('comment_moderation','r') == 'none') {
 				// Submit the comment
 				$dbh->query("INSERT INTO comments (pid,name,email,website,date,text) VALUES($comment_pid,'$comment_name','$comment_email','$comment_website',$comment_date,'$comment_text')") or die(sqlite_error_string($dbh->lastError));
-		}
-		// Send the user back to the page they came from
-		header('Location: '.bloginfo('url',2).'?post='.(int)$_POST['comment_pid']);
+			}
 	}
-	if(bloginfo('comment_moderation', 1) === 'approval') {
-	
-	}
+	// Send the user back to the page they came from
+	header('Location: '.bloginfo('url',2).'?post='.(int)$_POST['comment_pid']);
 }
 
 ?>
