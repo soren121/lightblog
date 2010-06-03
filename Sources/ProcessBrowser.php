1bb97454c6baaf8a3e1307789337a360946c4dee
doodle62 <doodle62@2a2979cc-7351-0410-a615-49a948d8c483>
2010-06-03 02:48:05 +0000

    Fixes issue 17; also finishes r482, which left post editing and comment submittal non-functional.

--- Sources/ProcessBrowser.php
+++ Sources/ProcessBrowser.php
@@ -26,12 +26,19 @@ if(isset($_POST['comment_submit'])) {
 	$query = $dbh->query("SELECT comments FROM posts WHERE id=".$comment_pid) or die(sqlite_error_string($dbh->lastError()));
 	if($query->fetchSingle() == '1') {
 		if(strlen($_POST['comment_name']) && strlen($_POST['comment_email']) > 0) {
+			// Require the HTML filter class
+			require('Class.InputFilter.php');
+			// Set allowed tags
+			$allowed_tags = array('b', 'i', 'u', 'em', 'strong', 'img', 'a', 'ul', 'ol', 'li', 'span', 'quote', 'br');
+			$allowed_attr = array('id', 'class', 'href', 'title', 'alt', 'style');
+			// Initialize class
+			$filter = new InputFilter($allowed_tags, $allowed_attr, 0, 0, 1);
 			// Escape values
-			$comment_name = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_name'])));
-			$comment_email = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_email'])));
-			$comment_website = sqlite_escape_string(strip_tags(cleanHTML($_POST['comment_website'])));
+			$comment_name = sqlite_escape_string(strip_tags($_POST['comment_name']));
+			$comment_email = sqlite_escape_string(strip_tags($_POST['comment_email']));
+			$comment_website = sqlite_escape_string(strip_tags($_POST['comment_website']));
 			$comment_date = time();
-			$comment_text = sqlite_escape_string(cleanHTML($_POST['comment_text']));
+			$comment_text = sqlite_escape_string($filter->process($_POST['comment_text']));
 			if(bloginfo('comment_moderation','r') == 'approval') {
 				// Submit the comment
 				$dbh->query("INSERT INTO comments (published,pid,name,email,website,date,text) VALUES(0,$comment_pid,'$comment_name','$comment_email','$comment_website',$comment_date,'$comment_text')") or die(sqlite_error_string($dbh->lastError));
