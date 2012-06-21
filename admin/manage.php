ae456e4cfe89f4ac52a116d85bdf7a90fb42b9c3
Nicholas Narsing <soren121@sorenstudios.com>
2012-06-21 16:00:37 +0000

    ! Fixed bugs in manage.php; it's not as bug-free as I thought it was :(

--- admin/manage.php
+++ admin/manage.php
@@ -25,15 +25,15 @@ if((int)$_GET['type'] == 1) { $type = 'posts'; }
 elseif((int)$_GET['type'] == 2) { $type = 'pages'; }
 elseif((int)$_GET['type'] == 3) { $type = 'categories'; }
 
-function loadrow($type, $count = 10, $before = null, $start = null)
+function loadrow($type, $count = 10, $before = 0, $start = 0)
 {
 	global $dbh;
-	if(!is_null($start))
+	if($start != 0)
 	{
 		$where = '';
 		$start = (int)(($start - 1) * $count);
 	}
-	elseif(!is_null($before))
+	else
 	{
 		$where = " WHERE id < ".(int)$before;
 		$start = 0;
@@ -177,9 +177,9 @@ $total = $total->numRows();
 							</tbody>
 						</table>
 					</form>
-					<div class="table-options">
-						<a href="javascript:loadpage('prev');" style="float:left;">&laquo; Prev Page</a>
-						<a href="javascript:loadpage('next');" style="float:right;">Next Page &raquo;</a>
+					<div class="table-options" style="height:20px;">
+						<a id="prev-link" href="javascript:loadpage('prev');" style="float:left;">&laquo; Prev Page</a>
+						<a id="next-link" href="javascript:loadpage('next');" style="float:right;">Next Page &raquo;</a>
 						<div class="clear"></div>
 					</div>
 					<p class="table-info">Showing <span id="row-start">1</span> - <span id="row-limit"></span> out of <span id="row-total"><?php echo $total ?></span> <?php echo $type ?>.</p>
@@ -192,7 +192,12 @@ $total = $total->numRows();
 		<script type="text/javascript" src="<?php bloginfo('url') ?>Sources/jQuery.Metadata.js"></script>
 		<script type="text/javascript">
 		//<![CDATA[
-			$('#manage').tablesorter(
+			if(window.location.hash == '')
+			{
+				window.location.hash = '#page=1';
+			}
+			
+			$('table').tablesorter(
 			{
 				widgets:['zebra', 'resizable'],
 				widgetOptions:
@@ -212,8 +217,37 @@ $total = $total->numRows();
 			
 			$('#itemnum').change(function()
 			{
-				loadpage('initial');
+				count = $('#itemnum > option:selected').val();
+				loadpage('reset');
 			});
+			
+			function pagination_callback()
+			{
+				var hash = window.location.hash;
+				var page = Number(hash.substr(6, 1));
+				var count = $('#itemnum > option:selected').val();
+				var rowtotal = Number($('span#row-total').text());
+				rowstart = (count * page) - count + 1;
+				$('span#row-start').text(rowstart);
+				if(rowstart == 1)
+				{
+					$('#prev-link').hide();
+				}
+				else
+				{
+					$('#prev-link').show();
+				}
+				if((count * page) >= rowtotal)
+				{
+					$('span#row-limit').text(rowtotal);
+					$('#next-link').hide();
+				}
+				else
+				{
+					$('#next-link').show();
+					$('span#row-limit').text(count * page);
+				}
+			}
 			
 			function loadrow_js(count, clear, start)
 			{
@@ -228,13 +262,13 @@ $total = $total->numRows();
 					{
 						if(clear == true)
 						{
-							$('#manage tbody').html(r.response);
+							$('table tbody').html(r.response);
 						}
 						else
 						{
-							$('#manage tbody').append(r.response);
+							$('table tbody').append(r.response);
 						}
-						$('#manage').trigger('update', [true]);
+						$('table').trigger('update', [true]);
 					}
 				}
 				var last = $('tbody tr.last').attr('id');
@@ -255,136 +289,184 @@ $total = $total->numRows();
 			
 			function loadpage(type)
 			{
-				if(window.location.hash == '')
-				{
-					window.location.hash = '#page-1';
-				}
+				$('#ajaxresponse').html('<img src="style/new/loading.gif" alt="Loading" />');		
+				
 				var hash = window.location.hash;
 				var page = Number(hash.substr(6, 1));
 				var count = $('#itemnum > option:selected').val();
 				var rowstart = Number($('span#row-start').text());
 				var rowlimit = Number($('span#row-limit').text());
-				var rowtotal = <?php echo $total ?>;
+				var rowtotal = Number($('span#row-total').text());
+				
 				if(type == 'prev')
 				{
 					if(page == 1)
 					{
+						$('#ajaxresponse').empty();
 						return;
 					}
 					page -= 1;
 					loadrow_js(count, true, page);
-					window.location.hash = '#page-' + String(page);
+					window.location.hash = '#page=' + String(page);
+					$('#next-link').show();
 				}
 				if(type == 'next')
 				{
-					if(page >= Math.round(rowtotal / count))
+					if(page >= (rowtotal / count))
 					{
+						$('#ajaxresponse').empty();
+						$('#next-link').hide();
 						return;
 					}
 					page += 1;
 					loadrow_js(count, true, page);
-					window.location.hash = '#page-' + String(page);
+					window.location.hash = '#page=' + String(page);
+					$('#prev-link').show();
 				}
 				if(type == 'initial')
 				{
 					loadrow_js(count, true, page);
 				}
-				$('span#row-start').text((count * page) - count + 1);
-				if((count * page) > rowtotal)
+				if(type == 'reset')
 				{
-					$('span#row-limit').text(rowtotal);
+					window.location.hash = '#page=1';
+					page = 1;
+					loadrow_js(count, true, page);
 				}
-				else
-				{
-					$('span#row-limit').text(count * page);
-				}
+				
+				pagination_callback();
+				$('#ajaxresponse').empty();
 			}
 			
 			loadpage('initial');
-
-			$(function()
+			
+			function deleterow_callback(r, single)
 			{
-				$('#bulk').submit(function()
+				if(r == null)
 				{
-					if($('#bulk select').val() == 'default')
+					$('#ajaxresponse').html('<p>AJAX request failed.</p>').css("color","#E36868");
+				}
+				else
+				{
+					if(r.result == 'success')
 					{
-						return false;
-					}
-					
-					var inputs = [];
-					$('.bf', this).each(function()
-					{
-						if($(this).is(':checkbox') && $(this).is(':not(:checked)'))
+						var action = $('select[name=action]').val();
+						if(action == 'delete' || single == true)
 						{
-							void(0);
+							$('#ajaxresponse').html('<p><?php echo ucwords($type) ?> deleted.</p>');
+							var checked = $('.table:checked').size();
+							$('.table:checked').parent('td').parent('tr').remove();
+							var rowtotal = Number($('span#row-total').text());
+							var rowlimit = Number($('span#row-limit').text());
+							$('span#row-total').text(rowtotal - checked);
+							if(rowlimit < rowtotal)
+							{
+								$('span#row-limit').text(rowlimit - checked);
+							}
+							var hash = window.location.hash;
+							var page = Number(hash.substr(6, 1));
+							if($('#searchTable tbody').children().length == 0 && page > 1)
+							{
+								loadpage('prev');
+								$('#next-link').hide();
+								return;
+							}
+							loadrow_js(checked, false, 0);
+							$('table').trigger('update', [true]);
+							pagination_callback();
 						}
 						else
 						{
-							inputs.push(this.name + '=' + this.value);
+							$('#ajaxresponse').html('<p><?php echo ucwords($type) ?> updated.</p>');
+							$('.table:checked').parent().next().children('span').remove();
 						}
-					});
-					
-					$('#ajaxresponse').html('<img src="<?php bloginfo('url') ?>admin/style/new/loading.gif" alt="Saving" />');
+						if(action == 'unpublish')
+						{
+							$('.table:checked').parent().next().append(' <span style="color:#E36868;">&mdash; Draft</span>');
+						}
+						$('select[name=action]').val('default');
+						$('.table:checked').attr('checked', 'false');
+					}
+					else
+					{
+						$('#ajaxresponse').html('<p>AJAX request failed;<br />' + r.response + '</p>').css("color","#E36868");
+					}
+				}
+			}
 
+			$('#bulk').submit(function()
+			{
+				if($('#bulk select').val() == 'default')
+				{
+					return false;
+				}
+
+				var inputs = [];
+				$('.bf', this).each(function()
+				{
+					if($(this).is(':checkbox') && $(this).is(':not(:checked)'))
+					{
+						void(0);
+					}
+					else
+					{
+						inputs.push(this.name + '=' + this.value);
+					}
+				});
+
+				$('#ajaxresponse').html('<img src="style/new/loading.gif" alt="Saving" />');
+
+				jQuery.ajax(
+				{
+					data: inputs.join('&'),
+					type: "POST",
+					url: $(this).attr('action'),
+					timeout: 2000,
+					error: function()
+					{
+						$('#ajaxresponse').html('AJAX request failed.').css("color","#E36868");
+					},
+					dataType: 'json',
+					success: function(data)
+					{
+						deleterow_callback(data, false);
+					}
+				})
+				return false;
+			});
+			
+			function deleteItem(id, title)
+			{
+				var type = 'category';
+				if('<?php echo $type ?>' != 'categories')
+				{
+					type = '<?php echo $type ?>';
+					type = type.substr(0, type.length - 1);
+				}
+				var answer = confirm('Do you really want to delete ' + type + ' "' + title + '"?');
+				if(answer)
+				{
+					$('#ajaxresponse').html('<img src="<?php bloginfo('url') ?>admin/style/new/loading.gif" alt="Saving" />');
+					
+					$('tr#' + id + ' > td:first').children(':checkbox').attr('checked', true);
 					jQuery.ajax(
 					{
-						data: inputs.join('&'),
+						data: "delete=true&csrf_token=<?php userinfo('csrf_token') ?>&type=<?php echo $type ?>&id=" + id,
 						type: "POST",
-						url: $(this).attr('action'),
+						url: "<?php bloginfo('url') ?>Sources/ProcessAJAX.php",
 						timeout: 2000,
+						dataType: 'json',
 						error: function()
 						{
-							$('#ajaxresponse').html('AJAX request failed.').css("color","#E36868");
+							$('#ajaxresponse').html('<p>Failed to delete ' + type + '.</p>').css("color","#E36868");
 						},
-						dataType: 'json',
-						success: function(r)
+						success: function(data)
 						{
-							if(r == null)
-							{
-								$('#ajaxresponse').html('<p>AJAX request failed.</p>').css("color","#E36868");
-							}
-							else
-							{
-								if(r.result == 'success')
-								{
-									var action = $('select[name=action]').val();
-									if(action == 'delete')
-									{
-										$('#ajaxresponse').html('<p><?php echo ucwords($type) ?> deleted.</p>');
-										var count = $('.table:checked').size();
-										$('.table:checked').parent('td').parent('tr').remove();
-										loadrow_js(count, false, 0);
-										$('#manage').trigger('update', [true]);
-										var rowtotal = Number($('span#row-total').text());
-										var rowlimit = Number($('span#row-limit').text());
-										$('span#row-total').text(rowtotal - 1);
-										if(rowlimit > rowtotal)
-										{
-											$('span#row-limit').text(rowlimit - 1);
-										}
-									}
-									else
-									{
-										$('#ajaxresponse').html('<p><?php echo ucwords($type) ?> updated.</p>');
-										$('.table:checked').parent().next().children('span').remove();
-									}
-									if(action == 'unpublish')
-									{
-										$('.table:checked').parent().next().append(' <span style="color:#E36868;">&mdash; Draft</span>');
-									}
-									$('select[name=action]').val('default');
-									$('.table:checked').attr('checked', 'false');
-								}
-								else
-								{
-									$('#ajaxresponse').html('<p>AJAX request failed;<br />' + r.response + '</p>').css("color","#E36868");
-								}
-							}
+							deleterow_callback(data, true);
 						}
 					})
-					return false;
-				})
-			});
+				}
+			}
 		//]]>
 		</script>
 
